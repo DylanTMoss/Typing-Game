61eb312e11e4c5844f62ad4d78cd59cee7277ac5
moss_899291 <moss_899291@VG162003A8144D.leanderisd.org>
2020-02-12 14:20:48 -0600

    fix javafx

---
+++ src/typecompeter/GuiController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package typecompeter;
+
+/**
+ *
+ * @author moss_899291
+ */
+public class GuiController {
+    
+    // create a profile
+    public void profileGoals() {
+	
+    }
+    
+    public void switchProfile() {
+	
+    }
+    
+    public void importText() {
+	
+    }
+}
