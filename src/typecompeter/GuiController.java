fc7101fb2f7afaf8000465e99ce538a708b20b65
moss_899291 <dmosscs@gmail.com>
2020-02-14 14:18:14 -0600

    add tables

--- src/typecompeter/GuiController.java
+++ src/typecompeter/GuiController.java
@@ -8,6 +8,11 @@ package typecompeter;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javax.swing.JFileChooser;
 
 
@@ -16,6 +21,9 @@ import javax.swing.JFileChooser;
  * @author moss_899291
  */
 public class GuiController {
+    public TableView tcnames;
+    public TableView tccharcts;
+    
     public void profileGoals() {
 	
     }
@@ -23,7 +31,7 @@ public class GuiController {
     public void switchProfile() {
 	
     }
-    
+
     public void importText() {
 	JFileChooser fc = new JFileChooser();
         int ret = fc.showOpenDialog(null);
@@ -33,9 +41,14 @@ public class GuiController {
             String[] parts = (f.getName()).split("\\.");
             if (s != null) {
                 Text t = new Text(parts[0],s);
+		addActiveText(t);
             } else {
                 //make error gui
             }
         }
+    }
+    
+    private void addActiveText(Text t) {
+	tcnames.
     }
 }
