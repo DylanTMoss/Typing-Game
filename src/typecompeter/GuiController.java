c93d3ace5a3afb43e5d2d2725160aa1570dd65f0
Dylan Moss <dmosscs@gmail.com>
2020-02-19 21:32:39 -0600

    start profile creation

--- src/typecompeter/GuiController.java
+++ src/typecompeter/GuiController.java
@@ -6,14 +6,22 @@
 package typecompeter;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import javax.swing.JFileChooser;
 
 
@@ -22,16 +30,23 @@ import javax.swing.JFileChooser;
  * @author moss_899291
  */
 public class GuiController {
+    @FXML private Pane main;
+    @FXML private TextField name_input;
+    @FXML private TextField key_input;
     @FXML private TableView textTable;
     @FXML private TableColumn tcnames;
     @FXML private TableColumn tcchars;
+    
     @FXML
     public void initialize() {
-	tcnames.setCellValueFactory(new PropertyValueFactory<>("name"));
-	tcchars.setCellValueFactory(new PropertyValueFactory<>("size"));
+	tcnames.setCellValueFactory(new PropertyValueFactory<Text,String>("name"));
+	tcchars.setCellValueFactory(new PropertyValueFactory<Text,Integer>("size"));
     }
-    public void profileGoals() {
-	
+    public void profileGoals() throws IOException {
+	Stage stage = new Stage();
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("ProfileCreation.fxml")));
+        stage.setScene(scene);
+        stage.show();
     }
     
     public void switchProfile() {
