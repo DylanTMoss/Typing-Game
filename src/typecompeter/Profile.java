cfe8b15c60de629896c8c44d6161479aaf3d4fa7
Dylan Moss <dmosscs@gmail.com>
2020-03-12 00:41:44 -0500

    add daily results tracking

--- src/typecompeter/Profile.java
+++ src/typecompeter/Profile.java
@@ -11,6 +11,7 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TimeZone;
+import java.util.TreeSet;
 
 /**
  *
@@ -19,14 +20,52 @@ import java.util.TimeZone;
 public class Profile implements Serializable{
     String name;
     int key;
-    Map<Integer, Result> dailyscores;
-    
+    TreeSet<Result> results;
+    int played;
+    int wins;
+
     public Profile (String name, int key) {
-        dailyscores = new HashMap();
+        results = new TreeSet();
         this.name = name;
         this.key = key;
     }
 
+    public Result getTop() {
+        return results.last();
+    }
+    
+    public void incrementWins() {
+        wins++;
+    }
+
+    public int getPlayed() {
+        return played;
+    }
+
+    public void setPlayed(int played) {
+        this.played = played;
+    }
+    
+    public int getCnt() {
+        return results.size();
+    }
+    
+    public int getAverage() {
+        double sum = 0;
+        for (Result r : results) {
+            sum += r.getWpm();
+        }
+        return (int) (sum / getCnt());
+    }
+
+    public TreeSet<Result> getResults() {
+        return results;
+    }
+    
+    public int getWins() {
+        return wins;
+    }
+    
     public String getName() {
 	return name;
     }
@@ -35,22 +74,35 @@ public class Profile implements Serializable{
 	return key;
     }
 
-    public Map<Integer, Result> getDailyscores() {
-	return dailyscores;
+    public Map<Integer, Integer> getDailyAverages() {
+	Map<Integer,Integer> dailyAvgs = new HashMap();
+        Map<Integer,Integer> uniqueDays = new HashMap();
+        Map<Integer,Integer> sums = new HashMap();
+        for (Result r : results) {
+            if (uniqueDays.containsKey(r.getDay())) {
+                uniqueDays.replace(r.getDay(), uniqueDays.get(r.getDay())+1);
+            } else {
+                uniqueDays.put(r.getDay(), 1);
+            }
+        }
+        for (Result r : results) {
+            if (sums.containsKey(r.getDay())) {
+                sums.replace(r.getDay(), sums.get(r.getDay())+1);
+                
+            } else {
+               sums.put(r.getDay(), r.getWpm());
+            }
+        }
+        for (int d : uniqueDays.keySet()) {
+            dailyAvgs.put(d,(int)(sums.get(d) * 1.0/uniqueDays.get(d)));
+        }
+        return dailyAvgs;
     }
     
     public void addScore(Result r) {
-        Calendar c = Calendar.getInstance(TimeZone.getDefault());
-        int date = c.get(Calendar.DATE);
-        if (dailyscores != null) {
-            if (dailyscores.containsKey(date)){
-            r.cnt++;
-            dailyscores.get(date).addWpm(r.getWpm());
-            } else {
-                r.cnt++;
-                dailyscores.put(date, r);
-            }
+        for (Result ra : results) {
+            System.out.println(ra);
         }
-        
+        results.add(r);
     }
 }
--- src/typecompeter/ProfileStatsController.java
+++ src/typecompeter/ProfileStatsController.java
@@ -5,10 +5,58 @@
  */
 package typecompeter;
 
+import java.util.Calendar;
+import java.util.Locale;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
 /**
  *
  * @author moss_899291
  */
 public class ProfileStatsController {
-    public Profile p;
+    public Profile prf;
+    @FXML private LineChart<Integer,Integer> lineChart_Profile;
+    @FXML private TableView tableView_topScores;
+    @FXML private Label label_averageWPM;
+    @FXML private Label label_wpmHigh;
+    @FXML private Label label_racesWon;
+    @FXML private Label label_racesPlayed;
+    @FXML private Label label_user;
+    
+    @FXML
+    public void initialize() {
+        
+    }
+    
+    public void setProfile(Profile p) {
+        this.prf = p;
+        loadData();
+    }
+
+    private void loadData() {
+        label_user.setText(prf.getName());
+        label_racesPlayed.setText("Races Played: " + prf.getPlayed());
+        label_racesWon.setText("Races Won: " + prf.getWins());
+        Calendar mCalendar = Calendar.getInstance();    
+        String month = mCalendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+        lineChart_Profile.getYAxis().setLabel("WPM");
+        lineChart_Profile.getXAxis().setLabel("Day of " + month + " " + mCalendar.get(Calendar.YEAR));
+        if (prf.getResults().size() > 0) {
+            label_wpmHigh.setText("Highest WPM: " + prf.getTop().getWpm());
+            label_averageWPM.setText("Average WPM: " + prf.getAverage());
+            XYChart.Series<Integer,Integer> chart = new XYChart.Series();
+            for (int i : prf.getDailyAverages().keySet()) {
+                chart.getData().add(new XYChart.Data<Integer,Integer>(i, prf.getDailyAverages().get(i)));
+            }
+            lineChart_Profile.setLegendVisible(false);
+            lineChart_Profile.getData().addAll(chart);
+        }
+        
+    }
 }
