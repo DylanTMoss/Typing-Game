b9ffe96cc3d0f57d7957b92a62d7452e78a8a949
Dylan Moss <dmosscs@gmail.com>
2020-03-12 00:45:54 -0500

    program now stops on exit

--- src/typecompeter/MainGui.java
+++ src/typecompeter/MainGui.java
@@ -7,6 +7,9 @@ package typecompeter;
 
 import java.io.IOException;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.stage.WindowEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -16,10 +19,22 @@ import javafx.stage.Stage;
 public class MainGui extends Application {
     
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("../fxml/MainGui.fxml"));
+        FXMLLoader loda = new FXMLLoader();
+        Parent root = loda.load(getClass().getResource("../fxml/MainGui.fxml").openStream());
+        GuiController gC = (GuiController) loda.getController();
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                for (Profile p : gC.loaded) {
+                    DataHandler.saveObject(p);
+                }
+                Platform.exit();
+                System.exit(0);
+            }
+        });
     }
 
     public static void main(String[] args) {
